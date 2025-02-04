c35bd4da434f7de3288bccb36f7c709966330c2d
GGTTGTGt <zz814170@gmail.com>
2025-02-05 00:55:26 +0700

    lab17_1 completed

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
 	int nA,nB;
-	int *A,*B,*C;	
+	int *A,*B,**C;	
 	
 	cout << "Length of A: ";
 	cin >> nA;
@@ -20,12 +20,12 @@ int main(){
 	cout << "Input Array B: ";
 	for(int i=0; i < nB; i++) cin >> B[i];
 	
-	C = new int;
-	for(int i=0; i < nA; i++) C[i] = new int;
-	
+	C = new int *[nA];
+	for(int i=0; i < nA; i++) C[i] = new int[nB];
+
 	for(int i=0; i < nA; i++){
 		for(int j=0; j < nB; j++) {
-			C = A[i]*B[j];
+			C[i][j] = A[i]*B[j];
 		}
 	}
 	
@@ -43,7 +43,14 @@ int main(){
 		}
 		cout << "\n";
 	}
-     delete A,B,C; 
+    delete A;
+	delete B;
+	for (int i = 0; i < nA; i++)
+	{
+		delete [] C[i];
+	}
+	
+	delete [] C; 
 
 	return 0;
 }
